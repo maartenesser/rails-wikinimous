e23096c0d1657578c743bc1814171193369d2934
Maarten Esser <maartenjr.esser@gmail.com>
2020-02-26 09:00:39 +0100

    updated gems and yarn

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Article.delete_all
+Article.create!(
+  [{
+    title: 'Hello world', content: 'this article wil talk about the first rails crud application'
+  },
+  {
+    title: 'Testing application', content: 'This article will talk about how to test your application'
+  },
+  {
+    title: 'Le Wagon', content: 'This acticle wil talk about Le Wagon.'
+  }
+])
